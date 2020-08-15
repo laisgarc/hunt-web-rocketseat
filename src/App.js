eb7b8c993aaf9fd5b85c23b6c6951f94e717b160
lais <laisgarcia@usp.br>
2020-08-15 14:52:47 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './routers';
+
+import './styles.css';
+
+import Header from './components/header';
+
+const App = () => (
+  <div className="App">
+  <Header />
+  <Routes />
+</div>
+);
+
+export default App;
